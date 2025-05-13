4fa8a08eb14ad80bfdf8a1a0becb93cad30f21c5
La1na <zolotukhinalp@gmail.com>
2025-05-13 12:47:03 +0200

    Add files via upload

---
+++ models/App.js
@@ -0,0 +1,23 @@
+// models/App.js
+module.exports = (sequelize, DataTypes) => {
+  const App = sequelize.define('App', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'Apps',
+    timestamps: false,
+  });
+
+  return App;
+};
